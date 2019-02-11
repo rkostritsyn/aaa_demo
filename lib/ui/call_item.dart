cfa1f6b911d9161bfe09f4fb01d72a53e2be3219
Roman Kostritsyn <rkostritsyn@griddynamics.com>
2019-02-11 02:02:28 +0200

    normal layout

--- lib/ui/call_item.dart
+++ lib/ui/call_item.dart
@@ -1,7 +1,6 @@
 import 'package:aaa_demo/res/style.dart';
 import 'package:aaa_demo/ui/screen/call_detail_screen.dart';
-import 'package:aaa_demo/ui/time_line/timeline.dart';
-import 'package:aaa_demo/ui/time_line/timeline_model.dart';
+import 'package:aaa_demo/ui/time_line/custom_time_line.dart';
 import 'package:flutter/material.dart';
 
 class CallItem extends StatefulWidget {
@@ -11,24 +10,21 @@ class CallItem extends StatefulWidget {
 }
 
 class CallItemState extends State<CallItem> {
-  double _animatedHeight = 100.0;
-  Widget _expandIcon = Icon(Icons.keyboard_arrow_down);
+  double _animatedHeight = 0.0;
+  Widget _expandIcon = Icon(Icons.keyboard_arrow_up);
 
-  final List<TimelineModel> list = [
-    TimelineModel(
-        id: "1",
-        description: "Test 1",
-        title: "Test 1"),
-    TimelineModel(
-        id: "2",
-        description: "Test 2",
-        title: "Test 2"),
-    TimelineModel(
-        id: "3",
-        description: "Test 3",
-        title: "Test 3")
+  List<TimeLineModel> my_steps = [
+    new TimeLineModel(
+      // Title of the Step
+        title: new Text("Step 1"),
+        subtitle: Text('SubTitle'),
+        isActive: true),
+    new TimeLineModel(
+        title: new Text("Step 2"),
+        subtitle: Text('SubTitle'),
+        // You can change the style of the step icon i.e number, editing, etc.
+        isActive: true),
   ];
-
 
   @override
   Widget build(BuildContext context) {
@@ -62,36 +58,125 @@ class CallItemState extends State<CallItem> {
                       ),
                     ),
 
-                    Container(
-                      height: 350,
-                      child: TimelineComponent(
-                        timelineList: list,
+                    //SPIKE FAKE-TIMELINE
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8),
+                      child: Column(
+                        children: <Widget>[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Container(
+                                width: 150,
+                                child: Text('7627 NW 127TH MANOR'),
+                              ),
+                              Container(
+                                  width: 100,
+                                  child: Text('NW 76TH ST TAMARAC, FL')),
+                            ],
+                          ),
+                          CustomStepper(
+                            steps: my_steps,
+                            type: TimeLineType.horizontal,
+                            tlColor: Colors.black12,
+                          ),
+                        ],
                       ),
                     ),
 
+                    Center(
+                      child: Text(
+                        'T 180 Tire Issue Request Tow To Shop',
+                        style: TextStyle(fontSize: 17, fontWeight: FontWeight.w600)), ),
+
                     AnimatedContainer(duration: const Duration(milliseconds: 120),
                       child: Container(
-                        height: 100,
-                        child:Text('Text')
+                        height: 130,
+                        child: getExpandedWidget()
                       ),
                       height: _animatedHeight,
                       width: double.infinity,
-                      color: Colors.tealAccent,
                     )
                   ],
                 ),
               )
           ),
 
-          getDivider(true)
+//          getDivider(true)
         ]
+    );
+  }
+
+  List<TimeLineModel> my_steps_full = [
+    new TimeLineModel(
+      // Title of the Step
+        title: new Text("Step 1"),
+        subtitle: Text('SubTitle'),
+        isActive: true),
+
+    new TimeLineModel(
+        title: new Text("Step 2"),
+        subtitle: Text('SubTitle'),
+        // You can change the style of the step icon i.e number, editing, etc.
+        isActive: true),
+
+    new TimeLineModel(
+        title: new Text("Step 2"),
+        subtitle: Text('SubTitle'),
+        // You can change the style of the step icon i.e number, editing, etc.
+        isActive: true),
+  ];
+
+  Widget getExpandedWidget() {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Text('Re 11:15'),
+              Text('Re 12:27'),
+              Text('Re 16:56'),
+            ],
+          ),
+
+          CustomStepper(
+            steps: my_steps_full,
+            type: TimeLineType.horizontal,
+          ),
+
+          Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(right: 10),
+                padding: EdgeInsets.all(5),
+                  child: Text('T6 Tow'),
+                decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder( side: BorderSide(color: Colors.blueAccent, width: 2
+                ) , borderRadius: BorderRadius.circular(15))),
+              ),
+
+              Container(
+                  child: Text('Passanger Car/Truck'),
+                padding: EdgeInsets.all(5),
+                decoration: ShapeDecoration(
+                    shape: RoundedRectangleBorder( side: BorderSide(color: Colors.blueAccent, width: 2
+                    ) , borderRadius: BorderRadius.circular(15))),
+              ),
+
+
+            ],
+          )
+        ],
+      ),
     );
   }
 
   void _expand() {
     setState((){
       _animatedHeight!=0.0?_animatedHeight=0.0:_animatedHeight=100.0;
-      _animatedHeight == 0.0? _expandIcon = Icon(Icons.keyboard_arrow_right) : _expandIcon = Icon(Icons.keyboard_arrow_down);
+      _animatedHeight == 0.0? _expandIcon = Icon(Icons.keyboard_arrow_up) : _expandIcon = Icon(Icons.keyboard_arrow_down);
     });
   }
 
@@ -106,14 +191,14 @@ class CallItemState extends State<CallItem> {
     }
   }
 
-  Widget getDivider(bool hasDivider) {
-    if(hasDivider) {
-      return Divider(height: 1, color: Colors.black54);
-    } else {
-      // SPIKE SPIKE SPIKE
-      return Divider(height: 1, color: Colors.transparent); ;
-    }
-  }
+//  Widget getDivider(bool hasDivider) {
+//    if(hasDivider) {
+//      return Divider(height: 1, color: Colors.black54);
+//    } else {
+//      // SPIKE SPIKE SPIKE
+//      return Divider(height: 1, color: Colors.transparent); ;
+//    }
+//  }
 
   Widget buildStatus(String text) {
     return CircleAvatar(
--- lib/ui/time_line/timeline_model.dart
+++
@@ -1,7 +0,0 @@
-class TimelineModel {
-  final String id;
-  final String title;
-  final String description;
-
-  const TimelineModel({this.id, this.title, this.description});
-}
